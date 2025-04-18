8d6ccc3809f1eaa946294f4a570c19738c682a6d
Amanda Meneghini <admcmarshmallow@gmail.com>
2025-04-18 08:08:59 -0300

    feat: adds @react-native-picker/picker and update App component
    Install the `@react-native-picker/picker` dependency for providing a
    select dropdown functionality. The App component has been updated
    to include and utilize this new Picker component for user interaction.

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import React from 'react';
+
 import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return(
     <View>
-      
+      <Text>TESTANDO</Text>
     </View>
   );
 }
